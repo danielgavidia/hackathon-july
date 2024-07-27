6c7ebfd6ee525a609dbb69a0b9f968a32222d4f2
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 13:05:08 -0400

    added functionality for API calls

--- front-end/src/components/Chat.tsx
+++ front-end/src/components/Chat.tsx
@@ -1,23 +1,38 @@
 import "../styles/chat.css";
 import { useState } from "react";
+import axios from "axios";
 
 const Chat = () => {
-    const [formData, setFormData] = useState("");
+    const [prompt, setPrompt] = useState<string>("");
 
-    // const handleSubmit= async(e: React.FormEvent<HTMLFormElement>) => {
-    //     e.preventDefault()
-    //     try {
-    //         const response = await fetch("")
-    //     }
-    // }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPrompt(e.target.value);
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const response = await axios(
+                `https://infinity-ai.fly.dev/prompt/${prompt}`
+            );
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div className="chat">
             <div>Ask about the horses</div>
             <div className="chat-box">
                 <div className="chat-box-output">output</div>
-                <form className="chat-box-input">
-                    <input className="chat-box-input-form" type="text" />
+                <form className="chat-box-input" onSubmit={handleSubmit}>
+                    <input
+                        className="chat-box-input-form"
+                        type="text"
+                        onChange={handleChange}
+                    />
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         </div>
