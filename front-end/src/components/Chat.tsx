a3d783578028865b05b96fa044659650e2c417cb
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 12:52:32 -0400

    added logic for pulling horse information

--- front-end/src/components/Chat.tsx
+++ front-end/src/components/Chat.tsx
@@ -1,16 +1,24 @@
 import "../styles/chat.css";
+import { useState } from "react";
 
 const Chat = () => {
+    const [formData, setFormData] = useState("");
+
+    // const handleSubmit= async(e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault()
+    //     try {
+    //         const response = await fetch("")
+    //     }
+    // }
+
     return (
         <div className="chat">
             <div>Ask about the horses</div>
             <div className="chat-box">
                 <div className="chat-box-output">output</div>
-                <div className="chat-box-input">
-                    <div className="chat-box-input-form">
-                        Write something here
-                    </div>
-                </div>
+                <form className="chat-box-input">
+                    <input className="chat-box-input-form" type="text" />
+                </form>
             </div>
         </div>
     );
