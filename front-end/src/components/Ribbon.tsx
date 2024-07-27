4f16b558314e76de577ab5a05dfccf8cf39ceef3
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 15:49:09 -0400

    new changes

---
+++ front-end/src/components/Ribbon.tsx
@@ -0,0 +1,32 @@
+import "../styles/navbar.css";
+import axios from "axios";
+
+const Ribbon = () => {
+    const handleNewGame = async () => {
+        try {
+            const response = await axios.get(
+                "https://infinity-ai.fly.dev/generate"
+            );
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div className="navbar">
+            <div className="title">Racing Application</div>
+            <div className="functionality">
+                <button className="new-game" onClick={() => handleNewGame()}>
+                    New Game
+                </button>
+                <div className="balance">Balance</div>
+                <div className="balance-amount">$100</div>
+                <div className="betting">Bet:</div>
+                <input className="betting-amount" type="number" />
+                <button className="race-start">Start</button>
+            </div>
+        </div>
+    );
+};
+
+export default Ribbon;
