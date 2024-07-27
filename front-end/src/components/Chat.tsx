1c278e1fddfe9f4626b5fec28c2be8415fc16b7a
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 13:27:37 -0400

    fixed chat functionality

--- front-end/src/components/Chat.tsx
+++ front-end/src/components/Chat.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const Chat = () => {
     const [prompt, setPrompt] = useState<string>("");
+    const [chat, setChat] = useState<Array<string>>([""]);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPrompt(e.target.value);
@@ -11,10 +12,13 @@ const Chat = () => {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setPrompt("");
+        setChat((chat) => [...chat, prompt]);
         try {
             const response = await axios(
                 `https://infinity-ai.fly.dev/prompt/${prompt}`
             );
+            setChat((chat) => [...chat, response.data.content]);
             console.log(response);
         } catch (error) {
             console.log(error);
@@ -25,10 +29,18 @@ const Chat = () => {
         <div className="chat">
             <div>Ask about the horses</div>
             <div className="chat-box">
-                <div className="chat-box-output">output</div>
+                <div className="chat-box-output">
+                    {chat.map((x) => (
+                        <>
+                            <p>{x}</p>
+                            <br />
+                        </>
+                    ))}
+                </div>
                 <form className="chat-box-input" onSubmit={handleSubmit}>
                     <input
                         className="chat-box-input-form"
+                        value={prompt}
                         type="text"
                         onChange={handleChange}
                     />
