b68ef60ff0a8d62d4358397a7d0ae938870731fe
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 15:01:00 -0400

    reverted to prior commit

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,13 +2,11 @@ import "./app.css";
 import Chat from "./components/Chat";
 import Navbar from "./components/Navbar";
 import Racing from "./components/Racing";
-import Ribbon from "./components/Ribbon";
 
 const App = () => {
     return (
         <div className="app">
             <Navbar />
-            <Ribbon />
             <Racing />
             <Chat />
         </div>
