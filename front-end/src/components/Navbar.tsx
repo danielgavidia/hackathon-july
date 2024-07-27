555b6840f71ee0f580501eb936bc46933d9a3247
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 14:57:12 -0400

    added ribbon

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -1,33 +1,7 @@
 import "../styles/navbar.css";
 import axios from "axios";
-import { useState } from "react";
 
 const Navbar = () => {
-    const [betInput, setBetInput] = useState<number | string>(0);
-    const [bet, setBet] = useState<number | string>(0);
-
-    console.log(`bet: ${bet}`);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        // If the input is empty, set betInput to an empty string
-        if (value === "") {
-            setBetInput("");
-        } else {
-            // Otherwise, convert the input to a number
-            setBetInput(Number(value));
-        }
-    };
-
-    const handleSetBetForm = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setBet(betInput);
-    };
-
-    const handleSetBet = () => {
-        setBet(betInput);
-    };
-
     const handleNewGame = async () => {
         try {
             const response = await axios.get(
@@ -44,20 +18,6 @@ const Navbar = () => {
             <div className="functionality">
                 <button className="new-game" onClick={() => handleNewGame()}>
                     New Game
-                </button>
-                <div className="balance">Balance</div>
-                <div className="balance-amount">$100</div>
-                <div className="betting">Bet:</div>
-                <form onSubmit={handleSetBetForm}>
-                    <input
-                        className="betting-amount"
-                        type="number"
-                        value={betInput}
-                        onChange={handleChange}
-                    />{" "}
-                </form>
-                <button className="race-start" onClick={handleSetBet}>
-                    Start
                 </button>
             </div>
         </div>
