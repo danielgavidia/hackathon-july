97cc592612460bd6e9846a37951f81e41405a826
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 13:59:31 -0400

    functionality fixes to chat

--- front-end/src/components/Chat.tsx
+++ front-end/src/components/Chat.tsx
@@ -1,10 +1,18 @@
 import "../styles/chat.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
+
+type InterfaceChat = {
+    name: string;
+    message: string;
+};
 
 const Chat = () => {
     const [prompt, setPrompt] = useState<string>("");
-    const [chat, setChat] = useState<Array<string>>([""]);
+    const [chat, setChat] = useState<Array<InterfaceChat>>([
+        { name: "", message: "" },
+    ]);
+    const endChatRef = useRef<HTMLDivElement>(null);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPrompt(e.target.value);
@@ -13,29 +21,53 @@ const Chat = () => {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setPrompt("");
-        setChat((chat) => [...chat, prompt]);
+        setChat((chat) => [...chat, { name: "user", message: prompt }]);
         try {
             const response = await axios(
                 `https://infinity-ai.fly.dev/prompt/${prompt}`
             );
-            setChat((chat) => [...chat, response.data.content]);
+            setChat((chat) => [
+                ...chat,
+                { name: "openai", message: response.data.content },
+            ]);
             console.log(response);
         } catch (error) {
             console.log(error);
         }
     };
 
+    useEffect(() => {
+        if (endChatRef.current) {
+            endChatRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "end",
+            });
+        }
+    });
+
     return (
         <div className="chat">
             <div>Ask about the horses</div>
             <div className="chat-box">
                 <div className="chat-box-output">
-                    {chat.map((x) => (
-                        <>
-                            <p>{x}</p>
-                            <br />
-                        </>
-                    ))}
+                    {chat.map((x) => {
+                        if (x.name === "user") {
+                            return (
+                                <>
+                                    <p>User: {x.message}</p>
+                                    <br />
+                                </>
+                            );
+                        } else if (x.name === "openai") {
+                            return (
+                                <>
+                                    <p>AI: {x.message}</p>
+                                    <br />
+                                </>
+                            );
+                        }
+                    })}
+                    <div ref={endChatRef}></div>
                 </div>
                 <form className="chat-box-input" onSubmit={handleSubmit}>
                     <input
