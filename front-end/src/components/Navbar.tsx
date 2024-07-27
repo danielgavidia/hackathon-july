fe81657feb4ad562908664b7c4015518372dfcc2
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 15:30:16 -0400

    changes

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -6,26 +6,26 @@ import axios from "axios";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [betInput, setBetInput] = useState<number | string>(0);
-  const [bet, setBet] = useState<number | string>(0);
+    const [betInput, setBetInput] = useState<number | string>(0);
+    const [bet, setBet] = useState<number | string>(0);
 
-  return (
-    <div className="navbar">
-      <div className="title">Racing Application</div>
-      <div className="functionality">
-        <button className="new-game" onClick={() => handleNewGame()}>
-          New Game
-        </button>
-        <div className="balance">Balance</div>
-        <div className="balance-amount">$100</div>
-        <div className="betting">Bet:</div>
-        <div className="betting-amount">$20</div>
-        <button className="race-start" onClick={onStartRace}>
-          Start
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="navbar">
+            <div className="title">Racing Application</div>
+            <div className="functionality">
+                <button className="new-game" onClick={() => handleNewGame()}>
+                    New Game
+                </button>
+                <div className="balance">Balance</div>
+                <div className="balance-amount">$100</div>
+                <div className="betting">Bet:</div>
+                <div className="betting-amount">$20</div>
+                <button className="race-start" onClick={onStartRace}>
+                    Start
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
