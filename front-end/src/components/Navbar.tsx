2f0d40d233e35049304086e5002ae2823a6c8cee
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-27 12:03:24 -0400

    CSS changes

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -4,6 +4,9 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div>Racing Application</div>
+            <div className="balance">Balance</div>
+            <div className="balance-amount">$100</div>
+            <div className="race-start">Start</div>
         </div>
     );
 };
